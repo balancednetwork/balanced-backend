9b9a8e2420fd4c3f615737a01c12722e253dbbea
robcxyz <rob@geometrylabs.io>
2022-06-05 01:23:05 -0600

    chore: update api tests

--- tests/integration/api/test_loans_chart.py
+++ tests/integration/api/test_loans_chart.py
@@ -5,10 +5,11 @@ from balanced_backend.config import settings
 
 
 def test_api_get_loans_chart(db: Session, client: TestClient):
-    response = client.get(f"{settings.REST_PREFIX}/preps")
+    response = client.get(
+        f"{settings.REST_PREFIX}/loans-chart?start_timestamp=1654409623&end_timestamp=1664409623&time_interval=84600")
     assert response.status_code == 200
 
 
 def test_api_get_loans_chart_error(db: Session, client: TestClient):
-    response = client.get(f"{settings.REST_PREFIX}?start_time=0")
-    assert response.status_code == 204
+    response = client.get(f"{settings.REST_PREFIX}/loans-chart?start_time=0")
+    assert response.status_code == 400
