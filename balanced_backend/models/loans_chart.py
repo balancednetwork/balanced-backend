ad831f96d9096cfb3d923ce92a95e13f06003bf1
robcxyz <rob@geometrylabs.io>
2022-06-09 08:15:45 -0600

    chore: add validator for loans chart

--- balanced_backend/models/loans_chart.py
+++ balanced_backend/models/loans_chart.py
@@ -1,14 +1,21 @@
+from datetime import datetime
 from typing import Optional
 
 from sqlalchemy.orm import declared_attr
-from sqlmodel import Field, SQLModel, Column, BigInteger
+from sqlmodel import Field, SQLModel
+from pydantic import validator
 
 
 class LoansChart(SQLModel, table=True):
-    # timestamp: Optional[int] = Field(default_factory=next_val, sa_column=Column(BigInteger(), primary_key=True, autoincrement=False))
     timestamp: Optional[int] = Field(default=None, primary_key=True)
+    datetime: datetime = None
+    value: Optional[float] = None
 
-    value: Optional[float] = Field(None, index=False)
+    @validator('datetime')
+    def update_datetime_field(cls, v, values):
+        if v is None:
+            return datetime.fromtimestamp(values['timestamp'])
+        return v
 
     class Config:
         extra = "ignore"
