a34143348a178aafeb45a41a8eb7d829ee094c72
robcxyz <rob@geometrylabs.io>
2022-06-20 09:05:26 -0600

    chore: nit to build

--- balanced_backend/alembic/env.py
+++ balanced_backend/alembic/env.py
@@ -70,28 +70,4 @@ async def run_migrations_online():
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
 
-
-# def run_migrations_online():
-#     configuration = config.get_section(config.config_ini_section)
-#     configuration["sqlalchemy.url"] = SQLALCHEMY_DATABASE_URL
-#     connectable = engine_from_config(
-#         configuration,
-#         prefix="sqlalchemy.",
-#         poolclass=pool.NullPool,
-#     )
-#
-#     with connectable.connect() as connection:
-#         context.configure(
-#             connection=connection, target_metadata=target_metadata, compare_type=True
-#         )
-#
-#         with context.begin_transaction():
-#             context.run_migrations()
-
-# if context.is_offline_mode():
-#
-#     run_migrations_offline()
-# else:
-#     asyncio.run(run_migrations_online())
-#     # run_migrations_online()
 asyncio.run(run_migrations_online())
