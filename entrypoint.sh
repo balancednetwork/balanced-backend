9b99a38f4a3f148fb3994fbae17ffd6ade66293b
robcxyz <rob@geometrylabs.io>
2022-06-20 11:14:05 -0600

    chore: update entrypoint to rm second arg until stream processing comes in

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,7 @@ if [ "$1" = "worker" ]; then
   cd balanced_backend
   alembic upgrade head
   echo "Starting worker..."
-  python main_worker.py "$2"
-elif [ "$1" = "cron" ]; then
-  echo "Migrating backend..."
-  cd balanced_backend
-  alembic upgrade head
-  echo "Starting cron..."
-  python worker/cron/$2.py
+  python main_worker.py
 elif [ "$1" = "api" ]; then
   echo "Starting API..."
   python balanced_backend/main_api.py
