531dae68a36d6f5e5eb59cf6d75cd5929154cabd
robcxyz <rob@sudoblock.io>
2022-08-22 19:10:44 -0600

    chore: add test back with fix in setup

--- tests/integration/worker/test_volume_worker.py
+++ tests/integration/worker/test_volume_worker.py
@@ -11,24 +11,24 @@ from balanced_backend.workers.volumes import (
 )
 
 
-# @pytest.mark.first
-# @freeze_time("2022-06-22")
-# def test_build_daily_volumes(db, loans_volumes_context):
-#     # Change update interval so it only does one update
-#     loans_volumes_context.start_timestamp = 1655723172
-#     loans_volumes_context.init_chart_time = 1655723172 - 86400
-#     loans_volumes_context.update_time()
-#     # 1655769600
-# 
-#     with db as session:
-#         build_volumes_time_series(
-#             session=session,
-#             context=loans_volumes_context,
-#         )
-#         result = session.execute(
-#             select(ContractMethodVolume).where(
-#                 ContractMethodVolume.address == loans_volumes_context.address)).scalars().all()
-#         assert result
+@pytest.mark.first
+@freeze_time("2022-06-22")
+def test_build_daily_volumes(db, loans_volumes_context):
+    # Change update interval so it only does one update
+    loans_volumes_context.start_timestamp = 1655723172
+    loans_volumes_context.init_chart_time = 1655723172
+    loans_volumes_context.update_time()
+
+    with db as session:
+        build_volumes_time_series(
+            session=session,
+            context=loans_volumes_context,
+        )
+        result = session.execute(
+            select(ContractMethodVolume).where(
+                ContractMethodVolume.address == loans_volumes_context.address)).scalars().all()
+        print(result)
+        assert result
 
 
 def test_set_table_value_from_time_period(db, loans_volumes_context):
