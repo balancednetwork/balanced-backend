82c91794aea35b246d4945ac70c601a2934947c2
robcxyz <rob@sudoblock.io>
2023-02-28 20:22:53 +0530

    chore: update pytest-ordering to pytest-order

--- tests/integration/api/test_api_cmc.py
+++ tests/integration/api/test_api_cmc.py
@@ -5,26 +5,26 @@ from sqlalchemy.orm import Session
 from balanced_backend.config import settings
 
 
-@pytest.mark.last
+@pytest.mark.order(-1)
 def test_api_get_cmc_summary(db: Session, client: TestClient):
     response = client.get(f"{settings.REST_PREFIX}/coin-market-cap/summary")
     assert response.status_code == 200
     assert len(response.json()) > 0
 
 
-@pytest.mark.last
+@pytest.mark.order(-1)
 def test_api_get_cmc_ticker(db: Session, client: TestClient):
     response = client.get(f"{settings.REST_PREFIX}/coin-market-cap/ticker")
     assert response.status_code == 200
 
 
-@pytest.mark.last
+@pytest.mark.order(-1)
 def test_api_get_cmc_orderbook(db: Session, client: TestClient):
     response = client.get(f"{settings.REST_PREFIX}/coin-market-cap/orderbook/sICX_ICX")
     assert response.status_code == 200
 
 
-@pytest.mark.last
+@pytest.mark.order(-1)
 def test_api_get_cmc_trades(db: Session, client: TestClient):
     response = client.get(f"{settings.REST_PREFIX}/coin-market-cap/trades/sICX_ICX")
     assert response.status_code == 200
--- tests/integration/cache/test_cache_cmc.py
+++ tests/integration/cache/test_cache_cmc.py
@@ -2,21 +2,21 @@ import pytest
 from balanced_backend.cache import cmc
 
 
-@pytest.mark.last
+@pytest.mark.order(-1)
 def test_update_cmc_summary(db):
     cmc.update_cmc_summary(db)
 
 
-@pytest.mark.last
+@pytest.mark.order(-1)
 def test_update_cmc_tickers(db):
     cmc.update_cmc_tickers(db)
 
 
-@pytest.mark.last
+@pytest.mark.order(-1)
 def test_update_cmc_order_book(db):
     cmc.update_cmc_order_book(db)
 
 
-@pytest.mark.last
+@pytest.mark.order(-1)
 def test_update_cmc_trades(db):
     cmc.update_cmc_trades(db)
