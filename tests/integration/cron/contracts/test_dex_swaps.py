ba46f1a6c9e835e32025aaae2d20fa01dca3562e
Rob Cannon <rob@sudoblock.io>
2024-10-29 13:00:02 +0700

    chore: change order of test to add swaps to db before another test runs

--- tests/integration/cron/contracts/test_dex_swaps.py
+++ tests/integration/cron/contracts/test_dex_swaps.py
@@ -1,9 +1,11 @@
+import pytest
 
 from balanced_backend.config import settings
 from balanced_backend.cron.contracts.dex_swaps import run_dex_swaps, get_last_swap
 from balanced_backend.utils.rpc import get_last_block
 
 
+@pytest.mark.order(1)
 def test_run_dex_swaps(db):
     # Usually there is a swap in last 1000 blocks otherwise need to mock get_last_block
     settings.BLOCK_SYNC_CHUNK = 1000
